98f13a76f158e57525aa92966b62965353dbb524
gorillaz1995 <104136116+gorillaz1995@users.noreply.github.com>
2024-08-24 17:03:44 +0300

    Fixed AnimatedText component and resolved issues

--- src/app/components/AnimatedText.tsx
+++ src/app/components/AnimatedText.tsx
@@ -8,30 +8,50 @@ const AnimatedText: React.FC = () => {
 
   useEffect(() => {
     gsap.to(textRefs.current, {
-      scale: 1.72, // Slight scale for emphasis
-      y: -5, // Slight vertical movement
+      scale: 1.72,
+      y: -5,
       duration: 0.5,
       ease: "power1.inOut",
       stagger: {
-        each: 1, // 1 second delay between each word's animation
+        each: 1,
       },
       repeat: -1,
-      repeatDelay: 1, // 1 second delay before the whole sequence repeats
+      repeatDelay: 1,
     });
   }, []);
 
   return (
     <div className={styles.container}>
-      <h2 ref={(el) => (textRefs.current[0] = el!)} className={styles.heading}>
+      <h2
+        ref={(el) => {
+          if (el) textRefs.current[0] = el;
+        }}
+        className={styles.heading}
+      >
         #1 CURS FRIZERIE
       </h2>
-      <h2 ref={(el) => (textRefs.current[1] = el!)} className={styles.heading}>
+      <h2
+        ref={(el) => {
+          if (el) textRefs.current[1] = el;
+        }}
+        className={styles.heading}
+      >
         LIDER IN INDUSTRIE
       </h2>
-      <h2 ref={(el) => (textRefs.current[2] = el!)} className={styles.heading}>
+      <h2
+        ref={(el) => {
+          if (el) textRefs.current[2] = el;
+        }}
+        className={styles.heading}
+      >
         REZULTATE REALE
       </h2>
-      <h2 ref={(el) => (textRefs.current[3] = el!)} className={styles.heading}>
+      <h2
+        ref={(el) => {
+          if (el) textRefs.current[3] = el;
+        }}
+        className={styles.heading}
+      >
         SUCCES GARANTAT
       </h2>
     </div>
